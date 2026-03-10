034ba845ed1686e5a225f0c6311b0d3f726af97d
noxie64 <the-real-nox@outlook.com>
2026-03-10 09:46:19 +0100

    Added notification-replacing. Doesnt work on wired-notify lol, wasted 1h of work. Also updated sys_batify

--- sys_notify.sh
+++ sys_notify.sh
@@ -14,9 +14,38 @@ positional argument:
 " "$0")
 
 OP=$1
+ID_DB="/tmp/sys_notify_current"
+TIMEOUT_LONG=1100
+TIMEOUT_SHORT=900
 
 fail() {
     echo "$HELP"
+}
+
+today () {
+    date +%d-%m-%Y
+}
+
+to_json() {
+    echo "{\"id\": $1, \"timeout\": $2, \"date\": \"$(today)\", \"millis\": $(date +%s)}"
+}
+
+notify () {
+    replace=0
+
+    if [ -e "$ID_DB" ]; then
+        id="$(jq .id $ID_DB)"
+        millis="$(jq .millis $ID_DB)"
+        timeout="$(jq .timeout $ID_DB)"
+        json_date="$(jq -r .date $ID_DB)"
+
+        echo "$(($millis - $(date +%s)))"
+        if [ "$json_date" == "$(today)" ] && [ $(($(date +%s) - millis)) -lt "$timeout" ]; then
+            echo "Replace ${id} $(($(date +%s) - millis))"
+            replace=1
+        fi
+    fi
+    to_json "$(notify-send $([ $replace -eq 1 ] && echo " --replace-id=$id ") -t "$1" --print-id "${@:2}")" "$1" > "$ID_DB"
 }
 
 ICONS_DEFAULT="$HOME/.config/wired/sys_notify/icons_default"
@@ -25,41 +54,41 @@ audio_out() {
     wpctl_output="$(wpctl get-volume @DEFAULT_SINK@)"
 
     if [ "$(echo "$wpctl_output" | grep -oF \[MUTED\])" = \[MUTED\] ]; then
-        notify-send "" "" --hint="string:wired-tag:sys_notify" \
-            -i "$ICONS_DEFAULT"/mute.png -t 900
+        notify "$TIMEOUT_SHORT" "" "" --hint="string:wired-tag:sys_notify" \
+            -i "$ICONS_DEFAULT"/mute.png
         exit 0
     fi
 
     volume="$(bc -l <<<"scale=0; ($(echo "$wpctl_output" | grep -oE "[[:digit:]]{0,2}\.[[:digit:]]{2}") * 100) / 1")"
 
     if [ "$volume" = "" ]; then
-        notify-send "" "" --hint="string:wired-tag:sys_notify" \
-            -i "$ICONS_DEFAULT"/unmute.png -t 900
+        notify "$TIMEOUT_SHORT" "" "" --hint="string:wired-tag:sys_notify" \
+            -i "$ICONS_DEFAULT"/unmute.png
         exit 0
     fi
 
-    notify-send "" "$volume" --hint="string:wired-tag:sys_notify" \
-        -i "$ICONS_DEFAULT"/volume_change.png -h int:value:"$volume" -t 1100
+    notify $TIMEOUT_LONG "" "$volume" --hint="string:wired-tag:sys_notify" \
+        -i "$ICONS_DEFAULT"/volume_change.png -h int:value:"$volume"
 }
 
 audio_in() {
     wpctl_output="$(wpctl get-volume @DEFAULT_SOURCE@)"
 
     if [ "$(echo "$wpctl_output" | grep -oF \[MUTED\])" = \[MUTED\] ]; then
-        notify-send "" "" --hint="string:wired-tag:sys_notify" \
-            -i "$ICONS_DEFAULT"/mic_mute.png -t 900
+        notify "$TIMEOUT_SHORT" "" "" --hint="string:wired-tag:sys_notify" \
+            -i "$ICONS_DEFAULT"/mic_mute.png
         exit 0
     fi
 
-    notify-send "" "$volume" --hint="string:wired-tag:sys_notify" \
+    notify "$TIMEOUT_SHORT" "" "$volume" --hint="string:wired-tag:sys_notify" \
         -i "$ICONS_DEFAULT"/mic.png
 }
 
 backlight() {
     brightness="$(brightnessctl info | grep -oE "([[:digit:]]{1,3}%)" | grep -oE "[[:digit:]]{1,3}")"
 
-    notify-send "" "$brightness" --hint="string:wired-tag:sys_notify" \
-        -i "$ICONS_DEFAULT"/light_full.png -h int:value:"$brightness" -t 1100
+    notify $TIMEOUT_LONG "" "$brightness" --hint="string:wired-tag:sys_notify" \
+        -i "$ICONS_DEFAULT"/light_full.png -h int:value:"$brightness"
 
 }
 
@@ -72,8 +101,8 @@ battery_level() {
     fi
 
     if [ "$capacity" -le 10 ]; then
-        notify-send "" "" -i "$ICONS_CRITICAL/battery.png" \
-            --hint="string:wired-tag:sys_notify" -t 1100
+        notify $TIMEOUT_LONG "" "" -i "$ICONS_CRITICAL/battery.png" \
+            --hint="string:wired-tag:sys_notify"
     fi
 }
 
@@ -81,13 +110,13 @@ charging_cable() {
     plugged_in="$(cat /sys/class/power_supply/AC/online)"
 
     if [ "$plugged_in" -eq 1 ]; then
-        notify-send "" "" -i "$ICONS_DEFAULT/plug.png" \
-            --hint="string:wired-tag:sys_notify" -t 1100
+        notify $TIMEOUT_LONG "" "" -i "$ICONS_DEFAULT/plug.png" \
+            --hint="string:wired-tag:sys_notify"
         exit 0
     fi
 
-    notify-send "" "" -i "$ICONS_DEFAULT/unplug.png" \
-        --hint="string:wired-tag:sys_notify" -t 1100
+    notify $TIMEOUT_LONG "" "" -i "$ICONS_DEFAULT/unplug.png" \
+        --hint="string:wired-tag:sys_notify"
     exit 0
 }
 
